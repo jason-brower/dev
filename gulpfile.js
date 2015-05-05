a34c3dc9d6237d2b39c17c24374f673fe05d7f79
Jason Brower <jasonb@allencomm.com>
2015-05-05 15:26:14 -0600

    Added Build JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 var gulp = require('gulp');
 var open = require('gulp-open');
 var gls = require('gulp-live-server');
+var path = require('path');
+var systemjs = require('systemjs-builder');
+
+gulp.task('build-js', function() {
+  var builder = new systemjs();
+  return builder.loadConfig('./config.js')
+    .then(function() {
+      builder.config({ baseURL: 'file:' + path.resolve('./') });
+      return builder.buildSFX('./app/app', './build.js', {
+        minify: true,
+        sourceMaps: true
+      });
+    });
+});
 
 gulp.task('webserver', function() {
   var server = gls.new('app.js');
